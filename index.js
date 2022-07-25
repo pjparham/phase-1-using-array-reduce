aea7457570edd4e32f67b52df25d9f2c2377c2c9
Parker Parham <parkerparham@gmail.com>
2022-07-25 11:16:35 -0400

    redid assignment with a single line function to pass tests

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
- let totalBatteries = batteryBatches.reduce(function(accumulator, currentValue){
-    return accumulator + currentValue;
- }, 0);
+//  let totalBatteries = batteryBatches.reduce(function(accumulator, currentValue){
+//     return accumulator + currentValue;
+//  }, 0);
 
- 
+//  let totalBatteries = batteryBatches.reduce(
+//     (accumulator, currentValue) => accumulator + currentValue
+//  , 0);
+
+ const totalBatteries = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
